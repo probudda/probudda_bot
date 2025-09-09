ca9a442b6b42ed0588480be44c095f95195bb445
probudda <probuddaom@gmail.com>
2025-09-09 05:30:23 +0300

    Update bot.py

--- TG BOT/bot.py
+++ TG BOT/bot.py
@@ -2,7 +2,30 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 # ВСТАВЬ СЮДА СВОЙ ТОКЕН! Скопируй его из @BotFather
-bot = telebot.TeleBot('1954706289:AAGYEZeBTxu3Ua64hKiOO3-w17I-Y0AOQgw')
+import os
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+# Безопасно получаем токен из переменной окружения
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Делаем кнопки, которые открывают сайты внутри Telegram
+keyboard = InlineKeyboardMarkup(row_width=2)
+btn1 = InlineKeyboardButton("YouTube", web_app=WebAppInfo(url='https://youtube.com'))
+btn2 = InlineKeyboardButton("Google", web_app=WebAppInfo(url='https://google.com'))
+btn3 = InlineKeyboardButton("Википедия", web_app=WebAppInfo(url='https://wikipedia.org'))
+btn4 = InlineKeyboardButton("GitHub", web_app=WebAppInfo(url='https://github.com'))
+btn5 = InlineKeyboardButton("Переводчик", web_app=WebAppInfo(url='https://translate.google.com'))
+btn6 = InlineKeyboardButton("Карты", web_app=WebAppInfo(url='https://yandex.ru/maps'))
+keyboard.add(btn1, btn2, btn3, btn4, btn5, btn6)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Привет! Я твой первый бот. Выбери кнопку (сайт откроется прямо здесь!):", reply_markup=keyboard)
+
+print("Бот начал работу! Он теперь всегда онлайн!")
+bot.infinity_polling()
 
 # Делаем кнопки, которые открывают сайты внутри Telegram
 keyboard = InlineKeyboardMarkup(row_width=2)
@@ -25,4 +48,5 @@ def send_welcome(message):
 
 # Включаем бота. Он будет постоянно "слушать" Telegram
 print("Бот начал работу! Чтобы остановить, нажми Ctrl+C в этом окне.")
+
 bot.infinity_polling()
