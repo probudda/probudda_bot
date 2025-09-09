fe7240f74d0a219d6c83b199d13d3a82d651fac0
probudda <probuddaom@gmail.com>
2025-09-09 06:26:53 +0300

    Update bot.py0

--- TG BOT/bot.py
+++ TG BOT/bot.py
@@ -8,18 +8,17 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 # Делаем кнопки, которые открывают сайты внутри Telegram
 keyboard = InlineKeyboardMarkup(row_width=2)
-btn1 = InlineKeyboardButton("Обзор каналов в телеграм", web_app=WebAppInfo(url='https://probudda.ru/telegram'))
-btn2 = InlineKeyboardButton("Карта буддийских храмов и центров", web_app=WebAppInfo(url='https://probudda.ru/map'))
-btn3 = InlineKeyboardButton("Тибетский календарь", web_app=WebAppInfo(url='https://tibetanbuddhistcalendar.org/day'))
-btn4 = InlineKeyboardButton("Сайт", web_app=WebAppInfo(url='https://probudda.ru/'))
-keyboard.add(btn1, btn2, btn3, btn4)
+btn1 = InlineKeyboardButton("YouTube", web_app=WebAppInfo(url='https://youtube.com'))
+btn2 = InlineKeyboardButton("Google", web_app=WebAppInfo(url='https://google.com'))
+btn3 = InlineKeyboardButton("Википедия", web_app=WebAppInfo(url='https://wikipedia.org'))
+btn4 = InlineKeyboardButton("GitHub", web_app=WebAppInfo(url='https://github.com'))
+btn5 = InlineKeyboardButton("Переводчик", web_app=WebAppInfo(url='https://translate.google.com'))
+btn6 = InlineKeyboardButton("Карты", web_app=WebAppInfo(url='https://yandex.ru/maps'))
+keyboard.add(btn1, btn2, btn3, btn4, btn5, btn6)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет!:", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Привет! Я твой первый бот. Выбери кнопку (сайт откроется прямо здесь!):", reply_markup=keyboard)
 
 print("Бот начал работу! Он теперь всегда онлайн!")
 bot.infinity_polling()
-
-
-
